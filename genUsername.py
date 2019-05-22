8f4c448b04d9b104bfcd1cb1cb1696f0e186c97f
WuZhiYi <wuzhiyi51@gmail.com>
2019-05-22 11:02:44 +0800

    update readme.txt

--- genUsername.py
+++ genUsername.py
@@ -2,8 +2,13 @@ import pandas as pd
 
 num = int(input("please input the number of username: "))
 
-cat = ['ptest'+'%03d'%i for i in range(0,num+1)]
+cat = ['ptest'+'%03d'%i for i in range(1,num+1)]
 
 dataframe = pd.DataFrame({'username':cat,'password':cat})
 
 dataframe.to_csv("test.csv",index=False,sep=',')
+
+li=[]
+for i in range(0,10):
+    li.append(i)
+print(li)
