1ebd311f5e4d18e0f1c11a71bbfcf3e476db158a
WuZhiYi <wuzhiyi51@gmail.com>
2019-05-24 10:38:37 +0800

    commit

--- genUsername.py
+++ genUsername.py
@@ -1,14 +1,20 @@
+# coding=utf-8
 import pandas as pd
 
-num = int(input("please input the number of username: "))
+num = int(input("请输入输入用户名数量（1-200）: "))
 
-cat = ['ptest'+'%03d'%i for i in range(1,num+1)]
+if num > 200:
+    print('您输入的数量大于200')
+else:
+    cat = ['ptest'+'%03d'%i for i in range(1,num+1)]
 
-dataframe = pd.DataFrame({'username':cat,'password':cat})
+    dataframe = pd.DataFrame({'username':cat,'password':cat})
 
-dataframe.to_csv("test.csv",index=False,sep=',')
+    dataframe.to_csv("user.csv",index=False,sep=',')
 
-li=[]
-for i in range(0,10):
-    li.append(i)
-print(li)
+    print('账号密码已生成，请查看user.csv文件')
+
+# li=[]
+# for i in range(0,10):
+#     li.append(i)
+# print(li)
