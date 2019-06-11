8fd12df2e9c5f2efe7f59f53f16cd313c7cc27aa
WuZhiYi <wuzhiyi51@gmail.com>
2019-06-11 17:13:19 +0800

    queue

---
+++ 每日一练/003.py
@@ -0,0 +1,20 @@
+# coding:utf-8
+
+#在迭代操作或者其他操作的时候，怎样只保留最后有限几个元素的历史记录？
+
+from collections import deque
+
+def search(lines, pattern, history=5):
+    previous_lines=deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+            yield line, previous_lines
+        previous_lines.append(line)
+
+if __name__ == "__main__":
+    with open(r'E:\learnpython\每日一练\somefile.txt') as f:
+        for line, prevlines in search(f, 'python', 5):
+            for pline in prevlines:
+                print(pline, end='')
+            print(line, end='')
+            print('-'*20)
